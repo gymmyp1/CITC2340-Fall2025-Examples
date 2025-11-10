e457aee26ecf8c63ea067b24172054b862aa302c
gymmyp1 <40799820+gymmyp1@users.noreply.github.com>
2025-11-10 16:35:39 +0000

    add notes to trigger syntax

--- module8-triggers/trigger_syntax.sql
+++ module8-triggers/trigger_syntax.sql
@@ -1,10 +1,10 @@
 -- General trigger syntax
 
 CREATE [OR REPLACE] TRIGGER trigger_name
-{BEFORE | AFTER | INSTEAD OF | FOR} trigger_event
+{BEFORE | AFTER | INSTEAD OF } trigger_event --indicates timing of when trigger fires. trigger_event is what What causes trigger to fire
 ON table_name
 [FOR EACH ROW]
-[WHEN (condition)]
+[WHEN (condition)] -- only applies to row-level triggers. tells oracle to only fire this trigger’s body for rows where this condition is true.
 BEGIN    
     trigger_body
 END trigger_name;
